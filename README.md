9ce5e5afebb0f6ff55d528174f59aac3932be910
Frank Rosner <frank.rosner@datastax.com>
2021-04-29 13:57:44 +0200

    app reloading config

--- README.md
+++ README.md
@@ -11,4 +11,12 @@ eval $(minikube docker-env)
 
 # Build image in minikube Docker daemon
 mvn compile jib:dockerBuild
+
+# Apply resources
+kubectl apply -f k8s/k8s-java-config-reload-configmap.yaml
+kubectl apply -f k8s/k8s-java-config-reload-pod.yaml
 ```
+
+## Resources
+
+- https://kubernetes.io/docs/tasks/configure-pod-container/configure-pod-configmap/#mounted-configmaps-are-updated-automatically
--- src/main/java/de/frosner/App.java
+++ src/main/java/de/frosner/App.java
@@ -1,9 +1,45 @@
 package de.frosner;
 
+import java.io.File;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.FileBasedConfiguration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.ReloadingFileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.commons.configuration2.reloading.PeriodicReloadingTrigger;
+
 public class App
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws InterruptedException
     {
-        System.out.println("Hello World!");
+        Parameters params = new Parameters();
+        File propertiesFile = new File("/config/config.properties");
+        ReloadingFileBasedConfigurationBuilder<FileBasedConfiguration> builder =
+                new ReloadingFileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
+                        .configure(params.fileBased()
+                                .setFile(propertiesFile));
+        PeriodicReloadingTrigger trigger = new PeriodicReloadingTrigger(builder.getReloadingController(),
+                null, 1, TimeUnit.SECONDS);
+        trigger.start();
+
+        try
+        {
+            while (true)
+            {
+                Configuration config = builder.getConfiguration();
+                String name = config.getString("name");
+                int sleepInterval = config.getInt("sleepIntervalMillis");
+                System.out.println(String.format("Hello %s, it is %s", name, new Date()));
+                Thread.sleep(sleepInterval);
+            }
+        }
+        catch (ConfigurationException cex)
+        {
+            System.err.println("Failed to load configuration: " + cex);
+        }
     }
 }
